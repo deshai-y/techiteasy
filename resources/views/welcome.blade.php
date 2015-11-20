66ef8067ef8d023218e1190a49f90a9891e6e392
Sylvain Collas <slvn.collas@gmail.com>
2015-11-20 14:47:05 +0100

    Add bootstrap and Html-Laravel-Provider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
     <head>
         <title>Tech'IT easy</title>
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        {!! Html::style('css/bootstrap.min.css') !!}
+        {!! Html::style('css/app.css') !!}
     </head>
     <body>
         <div class="container">
@@ -10,5 +12,7 @@
                 <div class="title">Laravel 5</div>
             </div>
         </div>
+        {!! Html::script('js/jquery-2.1.4.min.js') !!}
+        {!! Html::script('js/bootstrap.min.js') !!}
     </body>
 </html>
