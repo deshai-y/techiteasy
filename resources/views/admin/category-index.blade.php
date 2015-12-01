d56abe316500a27982182466867df1dd74e10cb0
Sylvain Collas <slvn.collas@gmail.com>
2015-12-01 14:20:12 +0100

    Add nav

--- resources/views/admin/category-index.blade.php
+++ resources/views/admin/category-index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <h1 class="page-header"><i class="fa fa-bookmark"></i> Catégories</h1>
 <ol class="breadcrumb">
-	<li><a href="{!! URL::previous() !!}"><i class="fa fa-arrow-circle-left"></i> Retour</a></li>
+	<li><a href="{!! route('dashboard') !!}"><i class="fa fa-tachometer"></i> Dashboard</a></li>
 	<li><a href="{!! route('admin.category.create') !!}"><i class="fa fa-plus-square"></i> Ajouter une catégorie</a></li>
 </ol>
 <table class="table table-striped">
